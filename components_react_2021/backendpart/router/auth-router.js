3a360db0b2e92c9b038b7c02417d2f3d761f4069
Rigzen <rigzenchosdon2510@gmail.com>
2024-07-07 23:49:43 +0530

    one2

--- components_react_2021/backendpart/router/auth-router.js
+++ components_react_2021/backendpart/router/auth-router.js
@@ -1,14 +1,24 @@
+
+
 const express = require("express");
 const router = express.Router();
-//router.get("/", (req,res)=>{
-//    res.status(200).send("welcome to pritis first website using router");
-//});
-router.route("/").get( (req,res)=>{
-    res.status(200).send("welcome to pritis first website using router");
-});
-router.route('/register').get((req,res)=>{
 
-});
+const authcontrollers = require("../controllers/auth-controllers");
+const schema = require("../validators/auth-validators");
+const validate = require("../middlewares/validate-middlewars");
+     
+router.route("/").get(authcontrollers.home);
+   
+router.route('/register')
+.post(validate(schema.signupSchema),authcontrollers.register);
+router.route('/login')
+.post(validate(schema.loginSchema),authcontrollers.login);
+// router.route("/").get( (req,res)=>{
+//     res.status(200).send("welcome to first website using router");
+// });
 
 
-module.exports = router
+module.exports = router;
+
+
+
